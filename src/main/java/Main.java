53b6d12ec0ac622d922aa84dcdbe47b28c7930e0
mohamedAY2004 <mido.yones1@gmail.com>
2025-07-05 16:22:38 +0300

    Modified the test case to verify the code handle more cases , feel free to try any invalid parameters yourself

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,53 @@
 import java.time.LocalDate;
-public class Main {
+import java.util.Scanner;
 
+public class Main {
 
     public static void main(String[]args){
        //The ProductBuilder is Reusable and clear values after each successful build
-       ProductBuilder builder=new ProductBuilder();
-       
+
+        Scanner sc=new Scanner(System.in);
+        ProductBuilder builder=new ProductBuilder();
+
        try {
            Product cheese=builder
                    .setName("Cheese")
                    .setPrice(6)
                    .setStock(8)
-                   .addTrait(new Expirable(LocalDate.of(2025,7,5)))
+                   .addTrait(new Expirable(LocalDate.of(2025,7,6)))
                    .addTrait(new Shippable(400))
                    .build();
-           
+
            Product biscuit=builder
                    .setName("Biscuit")
                    .setPrice(110)
                    .setStock(83)
                    .addTrait(new Expirable(LocalDate.of(2024,7,4)))
                    .build();
-           
+
            Product smartTV=builder
                    .setName("smartTV")
                    .setPrice(23500)
                    .setStock(1)
                    .addTrait(new Shippable(10705))
                    .build();
-           
+
            Product Phone=builder
                    .setName("SmartPhone")
                    .setPrice(17350)
                    .setStock(9)
                    .addTrait(new Shippable(500))
                    .build();
-           
+
            Product mobileScratch=builder
                    .setName("Mobile Scratch Card")
                    .setPrice(150)
                    .setStock(68)
                    .build();
-           
+
            Customer c=new Customer("Mohamed Younes",50000);
            Invoice i=new Invoice(c);
-           i.addItem(cheese,8);
+           i.addItem(cheese,9);
            i.addItem(biscuit,5);
            i.addItem(smartTV,1);
            i.removeItem(cheese);
@@ -52,7 +55,7 @@ public class Main {
            i.addItem(Phone,1);
            i.addItem(mobileScratch,1);
            i.checkOut();
-           
+
        } catch (IllegalArgumentException e) {
            System.err.println("Error creating products: " + e.getMessage());
        }
